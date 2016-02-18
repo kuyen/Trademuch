ab4b675aaca7a974f59e04fa706e03a814f32d03
gloria <gloria@gloriadeMacBook-Pro.local>
2016-02-18 14:58:06 +0800

    postDetail

--- assets/js/poDetailBack.js
+++ assets/js/poDetailBack.js
@@ -1,8 +1,10 @@
 $$(document).on('pageInit', '.page[data-page="postDetailF7"]', function(e) {
   $$('a.left.back.link').click(function() {
     console.log("!!!!!!");
-    window.location.href = '/main';
-    // mainView.router.loadPage('/main');
+    // window.location.href = '/main';
+    if(storyDetail){
+    mainView.router.loadPage('/main');
+  }
     // $("a.back.link :hover").css("color", "white");
   });
 });
