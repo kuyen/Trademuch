2f82c46bdab158fa6a8d1a021a58c02b57a5430e
gloria <gloria@gloriadeMacBook-Pro.local>
2016-02-25 11:32:45 +0800

    postDetail back回首頁

--- assets/js/postDetail.js
+++ assets/js/postDetail.js
@@ -1,12 +1,13 @@
 $$(document).on('pageInit', '.page[data-page="postDetailF7"]', function(e) {
   $$('a.left.back.link').click(function() {
     console.log("!!!!!!");
-    var history = mainView.history;
-    if(history.length-2){
+    var h = mainView.history;
+    if(h[h.length-2] == '#home'){
      mainView.router.back('/app');
-  }
+   }else {
+     mainView.router.back('/app');
+   }
     //  window.location.href = '/app';
-    // mainView.router.loadPage('/app');
-    // $("a.back.link :hover").css("color", "white");
+    //  mainView.router.loadPage('/app');
   });
 });
