471e726f5505c6df7848d6e97c4101181f930399
gloria <gloria@gloriadeMacBook-Pro.local>
2016-02-26 09:58:33 +0800

    postDetail rename變數

--- assets/js/postDetail.js
+++ assets/js/postDetail.js
@@ -1,8 +1,8 @@
 $$(document).on('pageInit', '.page[data-page="postDetailF7"]', function(e) {
   $$('a.left.back.link').click(function() {
     console.log("!!!!!!");
-    var h = mainView.history;
-    if(h[h.length-2] == '#home'){
+    var historyView = mainView.history;
+    if(historyView[historyView.length-2] == '#home'){
      mainView.router.back('/app');
    }else {
      mainView.router.back('/app');
