0a0477d4248ffdc8fae7a21199091dc53ff1822e
fuyaode <dan82625@gmail.com>
2016-03-24 15:49:17 +0800

    fix sessionAuth

--- config/policies.js
+++ config/policies.js
@@ -42,7 +42,7 @@ module.exports.policies = {
   },
   'PostController': {
     'getAll': [],
-    'create': [],
+    'create': ['sessionAuth'],
     'sqlSearch': [],
     'elasticSearch': [],
     'delete': [],
