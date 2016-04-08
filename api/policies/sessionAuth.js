82ea5f74fdef8d5429c505f69ec945d7a6082f48
fuyaode <dan82625@gmail.com>
2016-04-08 15:49:27 +0800

    response add status

--- api/policies/sessionAuth.js
+++ api/policies/sessionAuth.js
@@ -17,22 +17,27 @@ module.exports = async function(req, res, next) {
   // or if this is the last policy, the controller
   console.log('==== session ====');
   console.log(req.headers);
-  if(typeof req.body != "undefined" || typeof req.headers != "undefined") {
-    if(req.headers.jwt){
-      let user = await AuthService.jwtDecode(req.headers.jwt);
-      if(typeof user != "undefined"){
-        UserService.userToSession(user, req);
-        return next();
+  try {
+    if(typeof req.body != "undefined" || typeof req.headers != "undefined") {
+      if(req.headers.jwt){
+        let user = await AuthService.jwtDecode(req.headers.jwt);
+        if(typeof user != "undefined"){
+          await UserService.userToSession(user, req);
+          return next();
+        }
+      }
+      if(req.body.user) {
+        let {user} = req.body;
+        console.log('session user', user);
+        if(typeof user != "undefined"){
+          await UserService.userToSession(user, req);
+          return next();
+        }
       }
     }
-    if(req.body.user) {
-      let {user} = req.body;
-      console.log('session user', user);
-      if(typeof user != "undefined"){
-        UserService.userToSession(user, req);
-        return next();
-      }
-    }
+  } catch (e) {
+    console.log("forbidden!!!!!!!!!");
+    return res.forbidden();
   }
 
 
