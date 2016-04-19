d925769591b2c127b12ea2f90cc6dae25c7b8b58
Kuyen <iamcxa@gmail.com>
2016-04-19 15:26:29 +0800

    add model TradeRecord

---
+++ api/models/TradeRecord.js
@@ -0,0 +1,23 @@
+module.exports = {
+  attributes: {
+
+    type: {
+      type: Sequelize.ENUM('pedding', 'accepted', 'refused'),
+      defaultValue: 'pedding'
+    },
+
+  },
+
+  options: {
+    classMethods: {},
+    instanceMethods: {},
+    hooks: {},
+    underscored: true,
+    tableName: 'trade_record',
+    paranoid: true,
+  },
+  associations: function() {
+    TradeRecord.belongsTo(Post, {through: 'post_id'});
+    TradeRecord.belongsTo(User, {through: 'user_id'});
+  }
+};
